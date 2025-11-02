2e9163cb09581de739e70a5dcd184bb3b7053246
Raiyan Jiyon <raiyanjiyon814@gmail.com>
2025-11-02 20:28:49 +0600

    Add better response for books api according to project requirement

--- controllers/book.controller.ts
+++ controllers/book.controller.ts
@@ -1,3 +1,4 @@
+import { success } from 'zod';
 import Book from '../models/Book.ts';
 import { bookSchema } from '../validators/book.schema.ts';
 import { Request, Response } from 'express';
@@ -6,18 +7,42 @@ export const createBook = async (req: Request, res: Response) => {
     try {
         const parsedData = bookSchema.parse(req.body);
         const newBook = await Book.create(parsedData)
-        res.status(201).json(newBook);
+        res.status(201).json(
+            {
+                success: true,
+                message: 'Book created successfully',
+                data: newBook
+            }
+        );
     } catch (error) {
-        res.status(400).json({ message: 'Error creating book', error });
+        res.status(400).json(
+            {
+                success: false,
+                message: 'Error creating book',
+                error: error instanceof Error ? error.message : 'Unknown error'
+            }
+        );
     }
 };
 
 export const getBooks = async (req: Request, res: Response) => {
     try {
         const books = await Book.find();
-        res.status(200).json(books);
+        res.status(200).json(
+            {
+                success: true,
+                message: 'Books retrieved successfully',
+                data: books
+            }
+        );
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching books', error });
+        res.status(500).json(
+            {
+                success: false,
+                message: 'Error fetching books',
+                error: error instanceof Error ? error.message : 'Unknown error'
+            }
+        );
     }
 };
 
@@ -27,9 +52,21 @@ export const getBookById = async (req: Request, res: Response) => {
         if (!book) {
             return res.status(404).json({ message: 'Book not found' });
         }
-        res.status(200).json(book);
+        res.status(200).json(
+            {
+                success: true,
+                message: 'Book retrieved successfully',
+                data: book
+            }
+        );
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching book', error });
+        res.status(500).json(
+            {
+                success: false,
+                message: 'Error fetching book',
+                error: error instanceof Error ? error.message : 'Unknown error'
+            }
+        );
     }
 };
 
@@ -39,9 +76,21 @@ export const updateBook = async (req: Request, res: Response) => {
         if (!updateBook) {
             return res.status(404).json({ message: 'Book not found' });
         }
-        res.status(200).json(updateBook);
+        res.status(200).json(
+            {
+                success: true,
+                message: 'Book updated successfully',
+                data: updateBook
+            }
+        );
     } catch (error) {
-        res.status(400).json({ message: 'Error updating book', error });
+        res.status(400).json(
+            {
+                success: false,
+                message: 'Error updating book',
+                error: error instanceof Error ? error.message : 'Unknown error'
+            }
+        );
     }
 };
 
@@ -51,8 +100,18 @@ export const deleteBook = async (req: Request, res: Response) => {
         if (!deletedBook) {
             return res.status(404).json({ message: 'Book not found' });
         }
-        res.status(200).json({ message: 'Book deleted successfully' });
+        res.status(200).json( {
+            success: true,
+            message: 'Book deleted successfully',
+            data: null
+        });
     } catch (error) {
-        res.status(500).json({ message: 'Error deleting book', error });
+        res.status(500).json(
+            {
+                success: false,
+                message: 'Error deleting book',
+                error: error instanceof Error ? error.message : 'Unknown error'
+            }
+        );
     }
 }
